71a1f182a6e7e0ba944403a5e8dc7b1dff967914
Wealthometer <wealthhajoh87@gmail.com>
2025-12-15 13:13:09 +0100

    Create App.tsx

---
+++ frontend/App.tsx
@@ -0,0 +1,32 @@
+
+import 'react-native-reanimated'
+import 'react-native-gesture-handler'
+import { StyleSheet, } from 'react-native'
+import React from 'react'
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import RootNavigator from './src/navigation/RootNavigator';
+import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { store, persistor } from './src/store';
+import { PersistGate } from 'redux-persist/integration/react';
+const App = () => {
+  return (
+    <GestureHandlerRootView style={styles.safeAreaContainer}>
+      <NavigationContainer>
+        <Provider store={store} >
+          <PersistGate persistor={persistor}>
+            <RootNavigator />
+          </PersistGate>
+        </Provider>
+      </NavigationContainer>
+    </GestureHandlerRootView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  safeAreaContainer: {
+    flex: 1,
+  }
+})
