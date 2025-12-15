3e3ac69c7a3a947c7a82e304221cd5aca79bdb26
Wealthometer <wealthhajoh87@gmail.com>
2025-12-15 13:14:56 +0100

    Create MainTabsNavigation.tsx

---
+++ frontend/src/navigation/MainTabsNavigation.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import CartScreen from '../screens/CartScreen';
+import { TabNavigatorParamList } from '../types';
+import CustomTabBar from '../components/CustomTabBar';
+
+const MainTabsNavigation = () => {
+    const Tab = createBottomTabNavigator<TabNavigatorParamList>();
+
+    return (
+        <Tab.Navigator
+            // eslint-disable-next-line react/no-unstable-nested-components
+            tabBar={((props) => <CustomTabBar
+                onChange={() => { }}
+                data={[
+                    { label: "Home", name: 'House', route: 'Home' },
+                    { label: "Cart", name: 'ShoppingCart', route: 'Cart' },
+                    { label: "Profile", name: 'User', route: 'Profile' },
+                ]}
+                {...props} />)}
+            screenOptions={{ headerShown: false }}>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Cart" component={CartScreen} />
+            <Tab.Screen name="Profile" component={ProfileScreen} />
+        </Tab.Navigator>
+    )
+}
+
+export default MainTabsNavigation
